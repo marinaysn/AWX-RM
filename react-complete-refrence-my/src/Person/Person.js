9af0aa85e0c1b6fe93c736bb920e08c15647eef8
MarinaYsn <marina@bluelink.ca>
2020-01-06 01:07:10 -0500

    added radium element media to person comonent

--- react-complete-refrence-my/src/Person/Person.js
+++ react-complete-refrence-my/src/Person/Person.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Person.css'
 import Radium from 'radium';
 
 
 const person = (props) => {
+
+    const style ={
+        '@media (min-width: 900px)': {
+            'width': '450px',
+            'backgroundColor': 'rgb(232, 214, 248)'
+        }
+
+      
+    };
+
     return (
-        <div className='Person'>
+        <div className='Person' style={style}>
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
 
