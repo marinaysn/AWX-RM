d7381395b3dd2e3f947e13e79824cc00afb787e5
MarinaYsn <marina@bluelink.ca>
2020-01-12 10:27:21 -0500

    demo another way of using React.Fragment

--- react-complete-refrence-my/src/components/PersonList/Person/Person.js
+++ react-complete-refrence-my/src/components/PersonList/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './Person.css'
 import Auxiliary from '../../../hoc/Auxiliary'
 
@@ -23,13 +23,13 @@ class Person extends Component {
         console.log('Person.js - (4) render method...');
 
         return (
-            <React.Fragment>
+            <Fragment>
            {/*  <div className='Person' > */}
                 < p onClick={this.props.click} > I am {this.props.name} and I am {this.props.age} years old</p >
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
           {/*   </div> */}
-          </React.Fragment>
+          </Fragment>
         );
     }
 }
