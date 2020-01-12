24503b5d3415be4fd29bc60f7f13d2292b5b7f5a
MarinaYsn <marina@bluelink.ca>
2020-01-12 10:40:25 -0500

    added hoc withClasses to simplify use of className throughout the project

--- react-complete-refrence-my/src/components/PersonList/Person/Person.js
+++ react-complete-refrence-my/src/components/PersonList/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import './Person.css'
 import Auxiliary from '../../../hoc/Auxiliary'
 
@@ -23,13 +23,13 @@ class Person extends Component {
         console.log('Person.js - (4) render method...');
 
         return (
-            <Fragment>
+            <Auxiliary>
            {/*  <div className='Person' > */}
                 < p onClick={this.props.click} > I am {this.props.name} and I am {this.props.age} years old</p >
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
           {/*   </div> */}
-          </Fragment>
+          </Auxiliary>
         );
     }
 }
