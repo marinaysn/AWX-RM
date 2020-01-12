95b0b245d28ae11d9baaf3176d0e95f106862371
MarinaYsn <marina@bluelink.ca>
2020-01-12 09:45:20 -0500

    demo use Array as top level element

--- react-complete-refrence-my/src/components/PersonList/Person/Person.js
+++ react-complete-refrence-my/src/components/PersonList/Person/Person.js
@@ -21,13 +21,14 @@ class Person extends Component {
     render() {
         console.log('Person.js - (4) render method...');
 
-        return (
-            <div className='Person' >
-                < p onClick={this.props.click} > I am {this.props.name} and I am {this.props.age} years old</p >
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
-        );
+        return [
+           // <div className='Person' >
+                < p key="1" onClick={this.props.click} > I am {this.props.name} and I am {this.props.age} years old</p >
+               , <p key="i2" >{this.props.children}</p>
+               , <input  key="i3"  type="text" onChange={this.props.changed} value={this.props.name} />
+          //  </div>
+        
+        ]
     }
 }
 
