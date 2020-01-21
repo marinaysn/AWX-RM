0f8365a4c34b8836d5a6ad934f5f1958bd668bdc
MarinaYsn <marina@bluelink.ca>
2020-01-20 22:49:03 -0500

    added dynamic ingredient components

--- burgerbuilderapp/src/components/Layout/Layout.js
+++ burgerbuilderapp/src/components/Layout/Layout.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import Auxiliary from '../../hoc/Auxiliary'
-const Layout = (props) =>
-    (
+import React from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+//import classes from '../Layout/Layout.css'
+import './Layout.css';
+const Layout = (props) => {
+    return (
         <Auxiliary>
             <div>
                 ToolBar, SideDrawer, Backdrop
             </div>
-            <main>
+            <main className="mainContent">
                 {props.children}
             </main>
         </Auxiliary>
-
     )
+}
 
 export default Layout;
+
+
+
