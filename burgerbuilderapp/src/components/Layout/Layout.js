7f7634cd2c97059b106d7758d36492965c2de0fe
MarinaYsn <marina@bluelink.ca>
2020-01-26 23:32:52 -0500

    added Toolbar with css

--- burgerbuilderapp/src/components/Layout/Layout.js
+++ burgerbuilderapp/src/components/Layout/Layout.js
@@ -3,13 +3,12 @@ import Auxiliary from '../../hoc/Auxiliary';
 //import classes from '../Layout/Layout.css'
 import Burger from '../Burger/Burger'
 import './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const Layout = (props) => {
     return (
         <Auxiliary>
-            <div>
-                ToolBar, SideDrawer, Backdrop
-            </div>
+            <Toolbar />
             <main className="mainContent">
                 {props.children}
             </main>
