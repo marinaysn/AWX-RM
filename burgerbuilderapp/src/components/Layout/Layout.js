40ac0e2f153f08f1323e89a341f093df95f77ad6
MarinaYsn <marina@bluelink.ca>
2020-01-27 23:21:55 -0500

    started sidedrawer component implementation

--- burgerbuilderapp/src/components/Layout/Layout.js
+++ burgerbuilderapp/src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar'
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
 const Layout = (props) => {
     return (
         <Auxiliary>
             <Toolbar />
+            <Sidedrawer />
             <main className="mainContent">
                 {props.children}
             </main>
