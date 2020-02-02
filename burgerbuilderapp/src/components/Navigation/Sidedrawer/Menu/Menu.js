f8b24598552dc85990f565fb0e70deacffa57b1f
MarinaYsn <marina@bluelink.ca>
2020-02-01 20:28:44 -0500

    adding menu/css

--- burgerbuilderapp/src/components/Navigation/Sidedrawer/Menu/Menu.js
+++ burgerbuilderapp/src/components/Navigation/Sidedrawer/Menu/Menu.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Menu.css';
 
 const Menu = (props) => 
 
-  ( <div onClick={props.switchMenu} > 
-      Menu
+  ( <div onClick={props.switchMenu} className="Menu" >
+     <div></div>
+     <div></div>
+     <div></div>
    </div>)
 ;
 
