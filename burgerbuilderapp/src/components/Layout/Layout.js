8b27195fb2177bcff03afbe717a3408728e6194c
MarinaYsn <marina@bluelink.ca>
2020-02-01 19:55:50 -0500

    added sidedrawer toggle button

--- burgerbuilderapp/src/components/Layout/Layout.js
+++ burgerbuilderapp/src/components/Layout/Layout.js
@@ -5,31 +5,36 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
 class Layout extends Component {
-    state = {
-        showSideDrawer: true
-    }
+  state = {
+    showSideDrawer: false
+  };
 
+  sideDrawerClosedHandle = () => {
+    this.setState({
+      showSideDrawer: false
+    });
+  };
 
-    sideDrawerClosedHandle = () => {
-        this.setState({ 
-            showSideDrawer: false 
-        });
-    }
+  sideDrawerOpenHandle = () => {
+    this.setState((prevState) => {
+      return {
+        showSideDrawer: !prevState.showSideDrawer
+      };
+    });
+  };
 
-
-    render() {
-        return (
-            <Auxiliary>
-                <Toolbar />
-                <Sidedrawer closed={this.sideDrawerClosedHandle}
-                open = {this.state.showSideDrawer} />
-                <main className="mainContent">
-                    {this.props.children}
-                </main>
-            </Auxiliary>
-        );
-    }
+  render() {
+    return (
+      <Auxiliary>
+        <Toolbar switchMenuItems={this.sideDrawerOpenHandle} />
+        <Sidedrawer
+          closed={this.sideDrawerClosedHandle}
+          open={this.state.showSideDrawer}
+        />
+        <main className='mainContent'>{this.props.children}</main>
+      </Auxiliary>
+    );
+  }
 }
 
 export default Layout;
-
---
+++ burgerbuilderapp/src/components/Navigation/Sidedrawer/Menu/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Menu = (props) => 
+
+  ( <div onClick={props.switchMenu} > 
+      Menu
+   </div>)
+;
+
+export default Menu;
