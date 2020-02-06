e4f9a51695e5ee8dc3c9e0f84d13b77b8e8626c3
MarinaYsn <marina@bluelink.ca>
2020-02-06 00:14:35 -0500

    added dummy data and filter it

--- http01/src/containers/Blog/Blog.js
+++ http01/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,44 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state ={
+        posts: []
+    }
+
+    componentDidMount() {
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(responce =>{
+
+                const allPosts = responce.data.slice( 0,7);
+                const updatedPost =  allPosts.map(p =>{
+
+                    return{
+                        ...p,
+                    author: 'Marina'
+                    }
+                    
+                })
+                this.setState({posts: updatedPost})
+                console.log(responce);
+            });
+    }
+
     render () {
+
+        const posts = this.state.posts.map(post=>{
+            return <Post 
+                key={post.id} 
+                userId={post.userId}
+                id={post.id} 
+                title={post.title} 
+                body={post.body}
+                author={post.author}  />
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
