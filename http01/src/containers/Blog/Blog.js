8baa823e9fcbbd639f4d08c4b5ed4517d90ee083
MarinaYsn <marina@bluelink.ca>
2020-02-09 19:35:36 -0500

    createed and use axios instnances

--- http01/src/containers/Blog/Blog.js
+++ http01/src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -14,7 +15,7 @@ class Blog extends Component {
   };
 
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
+    axios.get('/posts')
     .then(responce => {
       const allPosts = responce.data.slice(0, 7);
       const updatedPost = allPosts.map(p => {
@@ -27,7 +28,7 @@ class Blog extends Component {
      // console.log(responce);
     })
     .catch( err =>{
-      console.log(err);
+      console.log('333333333333333');
       this.setState({error: true})
     })
     ;
--- http01/src/index.js
+++ http01/src/index.js
@@ -3,6 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+//axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authosization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(
+  req => {
+    // console.log(req);
+    return req;
+  },
+  error => {
+    console.log('1111111111111');
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  res => {
+    console.log(res);
+    return res;
+  },
+  error => {
+    console.log('2222222222');
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
