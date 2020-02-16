0a65bd2612a413a97bdc2a180dec1121ed233a93
MarinaYsn <marina@bluelink.ca>
2020-02-16 13:26:29 -0500

    added Error Handler routine

--- burgerbuilderapp/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgerbuilderapp/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 const INGREDIENTPRICES = {
   salad: 0.5,
@@ -185,4 +186,4 @@ export class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
--- burgerbuilderapp/src/hoc/withErrorHandler/withErrorHandler.js
+++ burgerbuilderapp/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import Auxilary from '../Auxilary/Auxiliary';
 
-const withErrorHandler = WrappedComponent => {
-  return props => {
-    return <WrappedComponent {...props} />;
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+
+    state ={
+      error: null
+    }
+
+    componentDidMount() {
+      axios.interceptors.request.use(req =>{
+        this.setState({error: null})
+        return req;
+      })
+      axios.interceptors.response.use(res => res, error =>{
+        this.setState({error: error});
+      })
+    }
+
+    errorConfirmedHandler =() =>{
+      this.setState({error: null})
+    }
+
+    render() {
+      return (
+        <Auxilary>
+          <Modal show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+            >
+              {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />;
+        </Auxilary>
+      );
+    }
   };
 };
 
