cfd64d82128755e1da9c77705f8ea88120d715ba
MarinaYsn <marina@bluelink.ca>
2020-03-01 01:26:22 -0500

    fixed few bugs, with timer and number selection

--- a4stars/src/components/StarMatch/StarMatch.js
+++ a4stars/src/components/StarMatch/StarMatch.js
@@ -16,10 +16,14 @@ const StarMatch = props => {
   const gameWon = avaialbleNumbers.length === 0;
   const candidateWrong = utils.sum(candidateNumbers) > stars;
   const gameLost = secondsLeft === 0;
+  const gameStatus = avaialbleNumbers.length === 0 
+    ? 'won' 
+    : secondsLeft === 0 ? 'lost' : 'active';
+
 
   //setTimmer
   useEffect(() => {
-    if (secondsLeft > 0) {
+    if (secondsLeft > 0 && avaialbleNumbers.length > 0) {
     const timerId = setTimeout(() => {
         setSecondsLeft(secondsLeft - 1);
       }, 1000);
@@ -38,7 +42,7 @@ const StarMatch = props => {
   };
 
   const onNumberClickHandler = (numberClicked, currentNumberStatus) => {
-    if (currentNumberStatus == 'used') {
+    if (gameStatus !== 'active' || currentNumberStatus == 'used') {
       return;
     }
 
@@ -63,19 +67,19 @@ const StarMatch = props => {
     setStars(utils.random(1, 9));
     setAvaialbleNumbers(utils.range(1, 9));
     setCandidateNumbers([]);
-
-    console.log(stars);
+    setSecondsLeft(10);
   };
 
-  const starsShow = gameWon ? (
-    <PlayAgain onClick={startNewGameHandler} />
+  const starsShow = gameStatus !== 'active' ? (
+    <PlayAgain onClick={startNewGameHandler}
+    gameStatus ={gameStatus} />
   ) : (
     <StarsDisplay count={stars} />
   );
 
-  const gameComment = gameLost ? (
+  const gameComment = gameStatus === 'lost' ? (
     <p>You lost!</p> 
-  ) : null
+  ) : gameStatus === 'won' ? <p>You Won!</p>  : null
 
   return (
     <div className='game'>
@@ -107,41 +111,3 @@ const StarMatch = props => {
 };
 
 export default StarMatch;
-
-// const colors = {
-//   available: 'lightgray',
-//   used: 'lightgreen',
-//   wrong: 'lightcoral',
-//   candidate: 'deepskyblue'
-// };
-
-// // Math science
-// const utils = {
-//   // Sum an array
-//   sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
-
-//   // create an array of numbers between min and max (edges included)
-//   range: (min, max) =>
-//     Array.from({ length: max - min + 1 }, (_, i) => min + i),
-
-//   // pick a random number between min and max (edges included)
-//   random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
-
-//   // Given an array of numbers and a max...
-//   // Pick a random sum (< max) from the set of all available sums in arr
-//   randomSumIn: (arr, max) => {
-//     const sets = [[]];
-//     const sums = [];
-//     for (let i = 0; i < arr.length; i++) {
-//       for (let j = 0, len = sets.length; j < len; j++) {
-//         const candidateSet = sets[j].concat(arr[i]);
-//         const candidateSum = utils.sum(candidateSet);
-//         if (candidateSum <= max) {
-//           sets.push(candidateSet);
-//           sums.push(candidateSum);
-//         }
-//       }
-//     }
-//     return sums[utils.random(0, sums.length - 1)];
-//   }
-// };
