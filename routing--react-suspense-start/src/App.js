77c1305592c0662d996a6557c9f71a7ba32ba7d1
MarinaYsn <marina@bluelink.ca>
2020-03-29 21:27:21 -0400

    added suspence loading

--- routing--react-suspense-start/src/App.js
+++ routing--react-suspense-start/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component, Suspense } from 'react';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
-
 // import Posts from './containers/Posts';
 import User from './containers/User';
 import Welcome from './containers/Welcome';
@@ -9,23 +8,43 @@ import Welcome from './containers/Welcome';
 const Posts = React.lazy(() => import('./containers/Posts'));
 
 class App extends Component {
+  state = {
+    status: false
+  };
+
+  onToggleHandler = () => {
+    this.setState(prev => {
+      return { status: !prev.status };
+    });
+  };
+
   render() {
     return (
-      <BrowserRouter>
-        <React.Fragment>
-          <nav>
-            <NavLink to="/user">User Page</NavLink> |&nbsp;
-            <NavLink to="/posts">Posts Page</NavLink>
-          </nav>
-          <Route path="/" component={Welcome} exact />
-          <Route path="/user" component={User} />
-          <Route path="/posts" render={() =>(
-            <Suspense fallback ={<div>Loading...</div>}>
-              <Posts />
-            </Suspense>
-          )} />
-        </React.Fragment>
-      </BrowserRouter>
+      <React.Fragment>
+        <button onClick={this.onToggleHandler}>Loading</button>
+        {this.state.status ? (
+          <User />
+        ) : (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Posts />
+          </Suspense>
+        )}
+      </React.Fragment>
+      // <BrowserRouter>
+      //   <React.Fragment>
+      //     <nav>
+      //       <NavLink to="/user">User Page</NavLink> |&nbsp;
+      //       <NavLink to="/posts">Posts Page</NavLink>
+      //     </nav>
+      //     <Route path="/" component={Welcome} exact />
+      //     <Route path="/user" component={User} />
+      //     <Route path="/posts" render={() =>(
+      //       <Suspense fallback ={<div>Loading...</div>}>
+      //         <Posts />
+      //       </Suspense>
+      //     )} />
+      //   </React.Fragment>
+      // </BrowserRouter>
     );
   }
 }
