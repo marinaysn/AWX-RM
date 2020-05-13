57d4ed496f3b84fc57260a0a37a18703b98665fa
MarinaYsn <marina@bluelink.ca>
2020-05-12 22:59:22 -0400

    adding error handling

--- newrbbapp/src/containers/BurgerBuilder/BurgerBuilder.js
+++ newrbbapp/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,7 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
-// import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENTPRICES = {
   salad: 0.75,
@@ -211,4 +211,4 @@ export class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder
+export default withErrorHandler(BurgerBuilder, axios);
