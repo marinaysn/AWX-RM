cff57ee089c791147690b3c0f14ec075d323b6d3
MarinaYsn <marina@bluelink.ca>
2020-05-14 01:23:31 -0400

    added styling to Posts

--- http02/src/containers/Blog/Posts/Posts.js
+++ http02/src/containers/Blog/Posts/Posts.js
@@ -1,70 +1,71 @@
 import React, { Component } from 'react';
 import Post from '../../../components/Post/Post'
 import axios from '../../../axios';
+import './Posts.css';
 
 export class Posts extends Component {
 
-  state ={
-    posts : [],
+  state = {
+    posts: [],
     currentPostID: null,
     error: false,
     errorMsg: ''
-}
+  }
 
-FullPostReviewHandler = async id =>{
+  FullPostReviewHandler = async id => {
 
-  await this.componentDidMount()
-  this.setState({ currentPostID:  id });  
-}
+    await this.componentDidMount()
+    this.setState({ currentPostID: id });
+  }
 
-componentDidMount() {
-  const postsTemp = [];
-  
-  axios.get('/posts')
-  .then(responce =>{
-      const temp = responce.data.splice(0,4);
-      const updatedPosts = temp.map( post =>{
+  componentDidMount() {
+    const postsTemp = [];
+
+    axios.get('/posts')
+      .then(responce => {
+        const temp = responce.data.splice(0, 4);
+        const updatedPosts = temp.map(post => {
           return {
-              ...post,
-              authors: "Marina"
+            ...post,
+            authors: "Marina"
           }
+        })
+
+        this.setState({ posts: updatedPosts });
+      })
+      .catch(err => {
+        this.setState({ error: true, errorMsg: err.toString() });
       })
 
-      this.setState({ posts: updatedPosts });
-  })
-  .catch(err =>{
-      this.setState({ error: true, errorMsg: err.toString() });
-  })
-  
-}
+  }
 
   render() {
 
-    let tempPosts = <p style={{textAlign: 'center'}}>Something is wrong</p>
+    let tempPosts = <p style={{ textAlign: 'center' }}>Something is wrong</p>
 
     let err = '';
-    if (!this.state.error){
+    if (!this.state.error) {
 
-        tempPosts = this.state.posts.map( p =>{
-         return <Post 
-                     key={p.id} 
-                     title={p.title} 
-                     author={p.authors} 
-                     clicked={() => this.FullPostReviewHandler(p.id)} />
-     })
+      tempPosts = this.state.posts.map(p => {
+        return <Post
+          key={p.id}
+          title={p.title}
+          author={p.authors}
+          clicked={() => this.FullPostReviewHandler(p.id)} />
+      })
     }
     else {
-     err = <p style={{textAlign: 'center'}}>{this.state.errorMsg}</p>
+      err = <p style={{ textAlign: 'center' }}>{this.state.errorMsg}</p>
     }
 
     return (
       <div>
         <section className="Posts">
-                    {tempPosts}
-                </section>
-                <section className="Posts">
-                    {err}
-                </section>
+          {tempPosts}
+        </section>
+        <section className="Posts">
+          {err}
+        </section>
       </div>
     )
   }
