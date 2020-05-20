8237fd6533f35ada68a3e047398dcec8c9fbadbc
MarinaYsn <marina@bluelink.ca>
2020-05-19 23:01:50 -0400

    testing use of Switch

--- http02/src/containers/Blog/Blog.js
+++ http02/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
-import { Route, NavLink } from "react-router-dom"
+import { Route, NavLink, Switch } from "react-router-dom"
 import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
@@ -31,20 +31,11 @@ class Blog extends Component {
 
                 </header>
 
-                
-                {/* <Route path="/" exact render={() => <Posts />} />
-                <Route path="/newpost" exact render={() => <NewPost />} /> */}
-
-                <Route path="/" exact component={Posts} />
-                <Route path="/newpost" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
-                
-                {/* <section>
-                    <FullPost id={this.state.currentPostID} />
-                </section>
-                <section>
-                    <NewPost />
-                </section> */}
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/newpost" component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
             </div>
         );
     }
