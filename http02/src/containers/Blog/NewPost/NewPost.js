a6067f6beefc40f74dcac17d1f06b58708b11c09
MarinaYsn <marina@bluelink.ca>
2020-05-20 23:59:16 -0400

    added redirect in new post

--- http02/src/containers/Blog/NewPost/NewPost.js
+++ http02/src/containers/Blog/NewPost/NewPost.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './NewPost.css';
+import { Redirect } from "react-router-dom";
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     }
     componentDidMount() {
         console.log('===============')
@@ -27,13 +29,21 @@ class NewPost extends Component {
 
         axios.post('/posts', post).then(response => {
                 console.log(response)
+                this.setState({ submitted:  true });
             })
     }
 
 
     render() {
+
+        let redirectState = null;
+
+        if(this.state.submitted){
+            redirectState =  <Redirect to="/postsmar" />
+        }
         return (
             <div className="NewPost">
+            {redirectState}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
