fcaaf05b63eb2b72f6aedca558d5b4be56a78a4f
MarinaYsn <marina@bluelink.ca>
2020-05-24 11:52:09 -0400

    added orders

---
+++ newrbbapp/src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Order.css'
+const Order =(props) =>{
+    return (
+        <div className='Order'>
+           <p>Ingredients: Salad (1) Meat (3) </p>
+           <p>Price <strong>CAN $5.45</strong></p>
+        </div>
+    )
+}
+
+export default Order ;
