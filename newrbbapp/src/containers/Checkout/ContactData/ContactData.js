d02bb146c6e4ffb400628bcb8e8ee31ad11cfaa1
MarinaYsn <marina@bluelink.ca>
2020-05-24 22:16:09 -0400

    adding custom Input element

--- newrbbapp/src/containers/Checkout/ContactData/ContactData.js
+++ newrbbapp/src/containers/Checkout/ContactData/ContactData.js
@@ -4,19 +4,59 @@ import './ContactData.css'
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import Input from '../../../components/UI/Input/Input'
 
 export class ContactData extends Component {
 
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            zipcode: '',
-            country: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email'
+                },
+                value: ''
+            },
+
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipcode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zip Code'
+                },
+                value: ''
+            },
+            country: 'Canada',
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [{value: 'uber', displayValue: 'Uber Eats'},
+                    {value: 'skip', displayValue: 'Skip the Dishes'},
+                    {value: 'ups', displayValue: 'UPS Standard'}]
+                },
+                value: ''
+            },
         },
         loading: false
     }
+
 
     orderHandler = (event) => {
         event.preventDefault();
@@ -30,16 +70,16 @@ export class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: this.state.name,
-                address: {
-                    street: this.state.street,
-                    zipCode: this.state.zipcode,
-                    country: 'Canada'
-                },
-                email: this.state.email
-            },
-            deliveryMethod: 'UPS Standard'
+            // customer: {
+            //     name: this.state.name,
+            //     address: {
+            //         street: this.state.street,
+            //         zipCode: this.state.zipcode,
+            //         country: 'Canada'
+            //     },
+            //     email: this.state.email
+            // },
+            // deliveryMethod: 'UPS Standard'
         };
 
         axios
@@ -68,10 +108,12 @@ export class ContactData extends Component {
 
         let form = (
             <form >
-                <input className='inp' type='text' value={this.state.name} onChange={e => this.handleChange(e)} name='name' placeholder='Your Name: ' />
-                <input className='inp' type='email' value={this.state.email} onChange={e => this.handleChange(e)} name='email' placeholder='Email: ' />
-                <input className='inp' type='text' value={this.state.street} onChange={e => this.handleChange(e)} name='street' placeholder='Your Street : ' />
-                <input className='inp' type='text' value={this.state.zipcode} onChange={e => this.handleChange(e)} name='zipcode' placeholder='Zip Code : ' />
+                <Input elementType='...' elementconfig='...' value='...' onChange={e => this.handleChange(e)} name='name'  />
+
+                
+                <Input inputtype='input' type='email' value={this.state.email} onChange={e => this.handleChange(e)} name='email' placeholder='Email: ' />
+                <Input inputtype='input' type='text' value={this.state.street} onChange={e => this.handleChange(e)} name='street' placeholder='Your Street : ' />
+                <Input inputtype='input' type='text' value={this.state.zipcode} onChange={e => this.handleChange(e)} name='zipcode' placeholder='Zip Code : ' />
                 <Button btnType='Success' click={this.orderHandler}>ORDER NOW</Button>
             </form>
 
