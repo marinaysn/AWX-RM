72aa954ffd1af30de40b77e73397df95696bf87a
MarinaYsn <marina@bluelink.ca>
2020-08-02 16:18:06 -0400

    added data with title and autour to post

--- http03c/src/containers/Blog/Blog.js
+++ http03c/src/containers/Blog/Blog.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +8,46 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state ={
+        posts: [],
+        authors: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.cypress.io/posts')
+        .then(responce =>{
+            this.setState({ posts:  responce.data});
+           // console.log(responce.data);
+        })
+
+        axios.get('https://jsonplaceholder.cypress.io/users')
+        .then(responce =>{
+            this.setState({ authors:  responce.data});
+           // console.log(responce.data);
+        })
+    }
+
+
     render () {
+
+        const posts = this.state.posts.map( post =>{
+           
+            let temp = this.state.authors.map( a => {
+                if (a.id = post.userId){
+                    return a.name
+                }
+                }
+           )
+
+            return <Post title={post.title} author={temp[post.userId]} />
+        }
+        )
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
